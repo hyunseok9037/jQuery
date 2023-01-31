87ed29725a49056041dcec925cd317b8265e3287
hyunseok <dopocoo@naver.com>
2023-01-31 16:50:41 +0900

    jquery

---
+++ ex02/funex01.js
@@ -0,0 +1,16 @@
+function m1() {
+
+}
+function m2() {
+    return 2;
+}
+function m3(a) {
+    console.log("앞에 m3");
+}
+let m4 = function () { };
+
+let m5 = () => { }
+let m6 = () => 1;
+let m7 = () => {
+    return 1;
+};
---
+++ ex02/varex01.js
@@ -0,0 +1,20 @@
+let n1 = 1;
+let d1 = 1.5;
+let b1 = true;
+let c1 = "가나";
+let arr = [1, 2, "가"]; //let arr = [];
+let user = {
+    id: 1,
+    username: "ssar",
+    email: "ssar@nate.com",
+    hobby: ["농구", "축구"],
+    account: { //user.id, user.hobby[1], user.account.id
+        id: 1122,
+        password: 8087,
+        balance: 50000
+    }
+};
+
+console.log("n1:" + n1);
+console.log("n1 : ", n1);
+console.log(`n1값은 ${arr} 입니다.`);
